49d94b6a3d39a26543ae1f06daca635dfb1fd6de
Lee-June-Kyu <leejuk95@naver.com>
2022-11-15 19:41:57 +0900

    Create WebAPI Code HumidityInfo

--- BackEnd/routes/web.js
+++ BackEnd/routes/web.js
@@ -21,7 +21,18 @@ router.get("/plant/:id", async (req, res,next) => {
 })
 
 router.get("/humidity/:id" , async (req, res)=> {
+  try {
+    console.log('req.params.id : ',req.params.id);
 
+    let humidityInfos = await Humidity.findAll({
+      where: { PlantId: req.params.id },
+    })
+    console.log(humidityInfos)
+    res.status(200).json({ success: true, data: humidityInfos })
+
+  } catch (err) {
+    res.status(400).json({ success: false, message : '해당 식물의 습도 정보가 없습니다.'})
+  }
 })
 
 module.exports = router;
